d03ec017477510e0bcabfa56d9414dfd74c4de8a
Michal Kosairz <michal.kosiarz@gmail.com>
2024-09-20 18:19:02 +0200

    Added parameters for script \n\n Parameters added: repository directory, start date, end date.

--- lines_of_code.bat
+++ lines_of_code.bat
@@ -1,12 +1,12 @@
 echo off
-
 setlocal
 
-set component=
-set start_date=
-set end_date=
+set component=%1
+set start_date=%2
+set end_date=%3
 
 cd %component%
+if errorlevel 1 goto end_of_script
 call :get_stats
 goto end_of_script
 
@@ -14,8 +14,7 @@ goto end_of_script
 for /f "delims=" %%i in ('call git rev-list -n 1 --before=%start_date% master') do set starting_commit=%%i
 for /f "delims=" %%i in ('call git rev-list -n 1 --before=%end_date master') do set ending_commit=%%i
 git diff --shortstat %starting_commit%..%ending_commit% -- . > nul
-
-if errorlevel 1 goto end_of_script
+if errorlevel 1 goto:eof
 
 for /f "delims=" %%i in ('git diff --shortstat %starting_commit%..%ending_commit% -- .') do set data=%%i 
 echo %component%: %data%
@@ -23,5 +22,7 @@ goto:eof
 
 :end_of_script
 cd ..
+
+endlocal
 echo on
  
